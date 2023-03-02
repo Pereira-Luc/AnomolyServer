2d9da93ab0985e9ec108081871559415502952f3
Pereira-Luc <luc.pereira.cardoso@gmial.com>
2023-03-02 20:34:30 +0100

    ADD SSL Self Signet

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,11 @@ import { ApolloServer } from '@apollo/server';
 import express from 'express';
 import * as http from "http";
 import {startStandaloneServer} from "@apollo/server/standalone";
+import {expressMiddleware} from "@apollo/server/express4";
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import fs from 'fs';
+import https from 'https';
 
 const schemas = makeExecutableSchema({
     resolvers: [resolvers],
@@ -16,26 +21,45 @@ const schemas = makeExecutableSchema({
 })
 
 const main = async () => {
-    const app = express();
-    const httpServer = http.createServer(app);
 
     const apolloServer = new ApolloServer({
         schema: schemas,
     });
 
-    const { url } = await startStandaloneServer(apolloServer, {
+    await apolloServer.start();
+
+    const app = express();
+
+
+    app.use('/graphql', cors<cors.CorsRequest>(), bodyParser.json(), expressMiddleware(apolloServer, {
         context: async ({ req }) => {
             const isLoggedIn = await isAuth(req);
             return { ...req, isLoggedIn };
-        },
+        }
+    }));
+
+
+    const httpServer = http.createServer(app);
+
+    const httpsServer = https.createServer({
+        key: fs.readFileSync('./certs/key.pem'),
+        cert: fs.readFileSync('./certs/cert.pem'),
+        passphrase: 'password',
+    }, app);
+
+    let PORT = 4000;
+
+    httpServer.listen(PORT, () => {
+        console.log(`HTTP Server running on port ${PORT}`);
     });
 
-    return url;
+    httpsServer.listen(4001, () => {
+        console.log(`HTTPS Server running on port 443`);
+    });
+
 }
 
-main().then( async (url) => {
-    console.log(`🚀 Server listening at: ${url}`);
-})
+main()
 
 
 
