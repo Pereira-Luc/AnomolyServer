d006eaf634dcb1a647577b6700aa77f642ceac24
Pereira-Luc <luc.pereira.cardoso@gmial.com>
2023-03-08 21:02:44 +0100

    Checks for all errors and if user exists

--- graphql/functions/signUp.ts
+++ graphql/functions/signUp.ts
@@ -1,9 +1,20 @@
 import {createUser, userExists} from "../../mongodb/functions/users";
 
-export const signUp = async (resolve: any, {username, password}: any, context: any) => {
+export const signUp = async (resolve: any, {username, password, confirmPassword}: any, context: any) => {
     //Check if the user exists
     let doesUserExist = await userExists(username);
-    if (doesUserExist !== null) { throw new Error("User already exists"); }
+
+
+    let errors = [];
+
+    if (doesUserExist) { errors.push("Username already exists"); }
+    if (username.length < 3) { errors.push("Username must be at least 3 characters long");}
+    if (password.length < 8) { errors.push("Password must be at least 8 characters long"); }
+    if (password !== confirmPassword) { errors.push("Passwords do not match"); }
+
+    if (errors.length > 0) {
+        throw new Error(errors.join(", "));
+    }
 
     //Create the user
     return await createUser(username, password);
