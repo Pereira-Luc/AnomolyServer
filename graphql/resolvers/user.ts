9d1a698fcc514889a5bf6f5b589324884e10f189
Pereira-Luc <luc.pereira.cardoso@gmial.com>
2023-03-08 21:03:27 +0100

    Sign up is a mutation now and add search user

--- graphql/resolvers/user.ts
+++ graphql/resolvers/user.ts
@@ -1,10 +1,18 @@
 import {Auth} from "../functions/Auth";
 import {signUp} from "../functions/signUp";
+import {searchUser} from "../functions/searchUser";
 
 export const UserResolvers = {
     Query: {
         login: async (resolve: any, {username, password}: any, context: any) => {
             return Auth(username, password);
+        },
+        searchUser: async (resolve: any, {v}: any, context: any) => {
+            //Check IF the user is authenticated
+            if (!context.isLoggedIn) { throw new Error("You are not authenticated");}
+            //Check if is empty remove spaces at the end and beginning
+            if (v.trim() === "") { return []; }
+            return await searchUser(v);
         },
         testLogin: async (resolve: any, parent: any, context: any) => {
             //Check IF the user is authenticated
@@ -14,9 +22,16 @@ export const UserResolvers = {
             }
             console.log('Authenticated');
             return "You are authenticated";
-        },signUp: async (resolve: any, {username, password}: any, context: any) => {
+        },
+        fetchTest: async (resolve: any, parent: any, context: any) => {
+            return "Fetching works";
+        }
+    },
+
+    Mutation: {
+        signUp: async (resolve: any, {username, password, confirmPassword, publicIdentityKey}: any, context: any) => {
             console.log('Signing up');
-            return await signUp(resolve, {username, password}, context);
+            return await signUp(resolve, {username, password, confirmPassword,publicIdentityKey}, context);
         }
     }
 }
