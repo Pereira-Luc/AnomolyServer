cd9d779986e8923ead194574dad9b25d1b748088
Pereira-Luc <luc.pereira.cardoso@gmial.com>
2023-03-09 02:26:41 +0100

    add: FriendRequestStatus, loadFriends, createFriends,acceptRequest

--- graphql/resolvers/user.ts
+++ graphql/resolvers/user.ts
@@ -1,6 +1,7 @@
 import {Auth} from "../functions/Auth";
 import {signUp} from "../functions/signUp";
 import {searchUser} from "../functions/searchUser";
+import {acceptFriendRequest, createFriends, getAllFriends} from "../functions/createFriends";
 
 export const UserResolvers = {
     Query: {
@@ -12,7 +13,12 @@ export const UserResolvers = {
             if (!context.isLoggedIn) { throw new Error("You are not authenticated");}
             //Check if is empty remove spaces at the end and beginning
             if (v.trim() === "") { return []; }
-            return await searchUser(v);
+            return await searchUser(v, context.userInfo.username);
+        },
+        loadFriends: async (resolve: any, {status}: any, context: any) => {
+            //Check IF the user is authenticated
+            if (!context.isLoggedIn) { throw new Error("You are not authenticated");}
+            return await getAllFriends(context.userInfo.username,status);
         },
         testLogin: async (resolve: any, parent: any, context: any) => {
             //Check IF the user is authenticated
@@ -32,6 +38,15 @@ export const UserResolvers = {
         signUp: async (resolve: any, {username, password, confirmPassword, publicIdentityKey}: any, context: any) => {
             console.log('Signing up');
             return await signUp(resolve, {username, password, confirmPassword,publicIdentityKey}, context);
+        },
+        createFriends: async (resolve: any, {friendUsername}: any, context: any) => {
+            //Check IF the user is authenticated
+            if (!context.isLoggedIn) { throw new Error("You are not authenticated");}
+            return await createFriends(context.userInfo.username, friendUsername);
+        },acceptRequest: async (resolve: any, {friendUsername}: any, context: any) => {
+            //Check IF the user is authenticated
+            if (!context.isLoggedIn) { throw new Error("You are not authenticated");}
+            return await acceptFriendRequest(context.userInfo.username, friendUsername);
         }
     }
 }
