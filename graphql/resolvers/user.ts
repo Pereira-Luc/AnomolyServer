1dd988ba2c5ddb747a042d286471ae93db7a8e5f
Pereira-Luc <luc.pereira.cardoso@gmial.com>
2023-03-15 21:09:23 +0100

    subscriptions

--- graphql/resolvers/user.ts
+++ graphql/resolvers/user.ts
@@ -2,7 +2,12 @@ import {Auth} from "../functions/Auth";
 import {signUp} from "../functions/signUp";
 import {searchUser} from "../functions/searchUser";
 import {acceptFriendRequest, createFriends, getAllFriends} from "../functions/createFriends";
-import {createChatRoom, sendMessage} from "../functions/chatsFunc";
+import {checkIfUserIsPartOfChat, createChatRoom, sendMessage} from "../functions/chatsFunc";
+import {withFilter} from "graphql-subscriptions";
+import {ObjectId} from "mongodb";
+import {PubSub} from "graphql-subscriptions";
+
+
 
 export const UserResolvers = {
     Query: {
@@ -10,6 +15,7 @@ export const UserResolvers = {
             return Auth(username, password);
         },
         searchUser: async (resolve: any, {v}: any, context: any) => {
+            console.log('Searching for user');
             //Check IF the user is authenticated
             if (!context.isLoggedIn) { throw new Error("You are not authenticated");}
             //Check if is empty remove spaces at the end and beginning
@@ -44,22 +50,39 @@ export const UserResolvers = {
             //Check IF the user is authenticated
             if (!context.isLoggedIn) { throw new Error("You are not authenticated");}
             return await createFriends(context.userInfo.username, friendUsername);
-        },acceptRequest: async (resolve: any, {friendUsername}: any, context: any) => {
+        },
+        acceptRequest: async (resolve: any, {friendUsername}: any, context: any) => {
             //Check IF the user is authenticated
             if (!context.isLoggedIn) { throw new Error("You are not authenticated");}
             return await acceptFriendRequest(context.userInfo.username, friendUsername);
         },
-        createChatRoom: async (resolve: any, {user}: any, context: any) => {
-            //Check IF the user is authenticated
-            if (!context.isLoggedIn) { throw new Error("You are not authenticated");}
-            return await createChatRoom(context.userInfo.username, user);
-        },
         sendMsg: async (resolve: any, {receiver, message}: any, context: any) => {
             //Check IF the user is authenticated
             if (!context.isLoggedIn) { throw new Error("You are not authenticated");}
-            return await sendMessage(context.userInfo.username, receiver, message);
+
+            return await sendMessage(context.userInfo.username, receiver, message, context.pubSub);
         }
+    },
 
+    Subscription: {
+        sendMsg: {
+            subscribe: async (_: any, {chatId}: any, {userInfo, pubSub}:any) => {
+                console.log('Subscribing to sendMsg');
+                if (!userInfo) {
+                    throw new Error("You are not authenticated");
+                }
+                console.log('User is authenticated');
+                console.log('Checking if user is part of chat');
+                console.log(chatId);
+                console.log(userInfo.username)
 
+                //Check if the user is part of the chat
+                if (!await checkIfUserIsPartOfChat(userInfo.username,new ObjectId(chatId))) {
+                    throw new Error("You are not part of this chat");
+                }
+
+                return pubSub.asyncIterator(['SEND_MSG']);
+            }
+        }
     }
 }
