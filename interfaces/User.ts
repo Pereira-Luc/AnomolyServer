4c58925ef5424656e8461e93b0034f241dfcda2e
Pereira-Luc <luc.pereira.cardoso@gmail.com>
2023-04-24 17:19:49 +0200

    add comment

--- interfaces/User.ts
+++ interfaces/User.ts
@@ -1,6 +1,10 @@
 import {FriendRequestStatus} from "./FriendRequestStatus";
 import {ObjectId} from "mongodb";
 
+
+/**
+ * This interface is used to define the User object
+ */
 export interface User {
     _id: ObjectId
     username: String
