dfa52dce4107670b35a7495e175c2ea4a96d6d5d
Pereira Luc <luc.pereira.001@student.uni.lu>
2023-05-28 01:21:36 +0200

    add close connection

--- mongodb/mongoConnection.ts
+++ mongodb/mongoConnection.ts
@@ -5,15 +5,23 @@ import { MongoClient, Db } from 'mongodb';
 
 // URI FOR MONGODB
 const uri: string = `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URI}/${process.env.MONGODB_DB} `;
-
+let client: MongoClient | null;
 let db: Db | null = null;
 
 export async function getDb(): Promise<Db> {
     if (!db) {
-        const client = new MongoClient(uri);
+        client = new MongoClient(uri);
         await client.connect();
         db = client.db(process.env.MONGODB_DB);
     }
 
     return db;
+}
+
+export async function closeDb(): Promise<void> {
+    if (client) {
+        await client.close();
+        client = null;
+        db = null;
+    }
 }
